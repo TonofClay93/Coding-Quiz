9c772adef671930e0feeabfe5e2c89c06b5e4f10
TonofClay93 <88801144+TonofClay93@users.noreply.github.com>
2021-09-14 09:57:07 -0400

    first code batch

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+//Initial elemtent vars
+var title = document.getElementById("title");
+
+var start = document.getElementById("start");
+
+var instruction = document.getElementById("instruction");
+
+var question = document.getElementById("question");
+
+var answers = document.getElementById("answers");
+
+var end = document.getElementById("end");
+
+var final = document.getElementById("final");
+
+var form = document.getElementById("form");
